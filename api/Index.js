3bd27dcca7df5ef9b8fb0062d4ffcd17a144fc11
SebastianM08 <2puntoscreativo@gmail.com>
2023-10-30 16:55:21 -0700

    CreatePost update

--- api/Index.js
+++ api/Index.js
@@ -28,10 +28,9 @@ app.post('/register', async (req,res) => {
       console.log(e);
       res.status(400).json(e);
     }
-         
-});
-
-app.post('/login', async (req,res) => {
+  });
+  
+  app.post('/login', async (req,res) => {
     const {username,password} = req.body;
     const userDoc = await User.findOne({username});
     const passOk = bcrypt.compareSync(password, userDoc.password);
@@ -45,17 +44,23 @@ app.post('/login', async (req,res) => {
         });
       });
     } else {
-      res.status(400).json('wrong credentials'); 
+      res.status(400).json('wrong credentials');
     }
-    app.get('/profile', (req,res) => {
-        const {token} = req.cookies;
-        jwt.verify(token, secret, {}, (err,info) => {
-          if (err) throw err;
-          res.json(info);
-        });
-      });
-      
   });
+  
+  app.get('/profile', (req,res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err,info) => {
+      if (err) throw err;
+      res.json(info);
+    });
+  });
+  
+  app.post('/logout', (req,res) => {
+    res.cookie('token', '').json('ok');
+  });
+
+  
 
 app.listen(4000); 
 
