c9812b24bd2d0826a26f64442860209686a29760
SebastianM08 <2puntoscreativo@gmail.com>
2023-10-30 15:53:01 -0700

    User & Create post added

---
+++ client/my-app/src/pages/CreatePost.js
@@ -0,0 +1,21 @@
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css';
+
+export default function CreatePost() {
+    return(
+        <form onSubmit={createNewPost}>
+      <input type="title"
+             placeholder={'Title'}
+             value={title}
+             onChange={ev => setTitle(ev.target.value)} />
+      <input type="summary"
+             placeholder={'Summary'}
+             value={summary}
+             onChange={ev => setSummary(ev.target.value)} />
+      <input type="file"
+             onChange={ev => setFiles(ev.target.files)} />
+      <Editor value={content} onChange={setContent} />
+      <button style={{marginTop:'5px'}}>Create post</button>
+    </form>
+    );
+}
