38eaa73407ccb59ca8d5ef51993af5dad4d7eeba
SebastianM08 <2puntoscreativo@gmail.com>
2023-10-30 06:53:56 -0700

    Homepage & Routing

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Post from "./Post"; 
+import Header from "./Header"
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div></div> 
+    <Routes>
+      <Route index element={
+        <main>
+          <Header/>
+          <Post/>
+          <Post/>
+          <Post/>
+        </main> 
+      } />
+    </Routes>
   );
 }
 
-export default App;
+export default App; 
---
+++ client/my-app/src/Post.js
@@ -0,0 +1,16 @@
+export default function Post() {
+    return (<div className="post">
+    <div className="image">
+    <img src="https://dancingastronaut.com/wp-content/uploads/2023/10/Screenshot-2023-10-13-at-12.13.13-PM.png" alt=""/>
+    </div>
+    <div className="texts">
+    <h2>The Beatport Group announces awardees of second annual Diversity + Parity Fund</h2>
+    <p className="info">
+      <a className="author">Sebastian Martinez</a>
+      <time>10-29-2023 12:45</time>
+    </p>
+    <p className="summary"> The electronic music community is filled with vibrant groups of creative people who want to make our industry and world better,” said Sofia Ilyas, Chief Community Officer of The Beatport Group. </p>
+    </div>
+  </div>
+        ); 
+}
